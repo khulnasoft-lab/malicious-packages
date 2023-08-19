86502dc862e7da98bbf86f8616a924a07525b851
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-19 07:54:47 +0600

    Update details_test.go

--- internal/report/details_test.go
+++ internal/report/details_test.go
@@ -1,18 +1,3 @@
-// Copyright 2023 Infected Packages Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-//nolint:goconst
 package report_test
 
 import (
@@ -23,7 +8,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/khulnasoft/infected-packages/internal/report"
+	"github.com/khulnasoft-lab/infected-packages/internal/report"
 )
 
 func reportWithDetail(details string) *report.Report {
